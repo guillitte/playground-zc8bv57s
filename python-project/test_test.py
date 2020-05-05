cf6b82f5a33e61af7d2a7b304438666f340164a4
Stéphane Guillitte <guillitte@users.noreply.github.com>
2020-05-05 13:28:50 +0200

    Update test_test.py

--- python-project/test_test.py
+++ python-project/test_test.py
@@ -14,7 +14,7 @@ def send_msg(channel, msg):
 
 
 def success():
-    print("TECHIO> open -s /project/target/ index.html")
+    print("TECHIO> open -s /project/target index.html")
     print("TECHIO> success true")
 
 
